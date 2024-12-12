5dc888e51b6124ee49d60d6e8309c8e041152b2b
scammi <scammi@gmail.com>
2024-12-12 13:14:14 -0300

    allow cors

--- src/app/api/certificates.ts
+++ src/app/api/certificates.ts
@@ -42,6 +42,18 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<CertificateResponse | ErrorResponse>
 ) {
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  );
+
+  // Handle OPTIONS request (preflight)
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
   if (req.method !== 'POST') {
     return res.status(405).json({
       error: 'METHOD_NOT_ALLOWED',
